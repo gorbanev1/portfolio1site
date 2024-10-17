d1cdfe571c9cc2ffc94e71452b57aca1c87bf819
Igor Gorbanev <gorbanev_i@mail.ru>
2024-10-17 14:08:30 +0300

    Contact

---
+++ src/layout/sections/contacts/Contacts.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Button} from "../../../components/Button";
+
+export const Contact = () => {
+    return (
+        <StyledContacts>
+            <SectionTitle>Contact</SectionTitle>
+            <StyledForm>
+                <Field placeholder={'name'}/>
+                <Field placeholder={'subject'}/>
+                <Field as={"textarea"} placeholder={'message'}/>
+                <Button type={'submit'}>Send Message</Button>
+            </StyledForm>
+        </StyledContacts>
+    );
+};
+
+const StyledContacts = styled.section`
+  min-height: 50vh;
+  background-color: #816464;
+`
+
+const StyledForm = styled.form`
+  max-width: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+const Field = styled.input`
+`
