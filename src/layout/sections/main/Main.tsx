80bee2b06e4429ae3d9e0dea0936df107a93c05f
Igor Gorbanev <gorbanev_i@mail.ru>
2024-10-21 15:21:35 +0300

    main 2 8 m

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import styled from "styled-components";
 import photo from "../../../assets/images/Myphoto.jpg"
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
 
 export const Main = () => {
     return (
         <div>
             <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <span>Hi there</span>
-                    <Name>I am Igor Gorbanev</Name>
-                    <MainTitle>A Web Developer</MainTitle>
-                </div>
-                <img src="./../../../assets/images/Myphoto.jpg" alt=""/>
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+                <Container>
+                    <FlexWrapper align={"center"} justify={"space-between"}>
+                        <div>
+                            <SmallText>Hi there</SmallText>
+                            <Name>I am Igor Gorbanev</Name>
+                            <MainTitle>A Web Developer</MainTitle>
+                        </div>
+                        <img src="./../../../assets/images/Myphoto.jpg" alt=""/>
+                        <Photo src={photo} alt=""/>
+                    </FlexWrapper>
+                </Container>
             </StyledMain>
         </div>
 
@@ -25,6 +28,7 @@ export const Main = () => {
 const StyledMain = styled.section`
   min-height: 100vh;
   background-color: rgba(220, 20, 60, 0.42);
+  display: flex;
 `
 
 const Photo = styled.img`
@@ -32,10 +36,19 @@ const Photo = styled.img`
   height: 430px;
   object-fit: cover;
 `
-const MainTitle = styled.h1
-`
+const MainTitle = styled.h1`
+  font-size: 27px;
+  font-weight: 400;
 `
 
-const Name = styled.h2
+const Name = styled.h2`
+  color: #FFF;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: 0.05em;
 `
+const SmallText = styled.h2`
+  font-size: 14px;
+  font-weight: 400;
 `
