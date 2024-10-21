e4dff1b1f4665954b666dd312c70213c6b862401
gorban <gorbanev_i@mail.ru>
2024-10-21 12:57:40 +0300

    Header 22

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from '../../components/logo/Logo';
-import Menu from "../../components/menu/Menu";
 import {Container} from "../../components/Container";
 import { FlexWrapper } from '../../components/FlexWrapper';
+import {HeaderMenu} from "./headermenu/HeaderMenu";
 
 export const items = ['Home','Skills','Works','Testimonials','Contact',]
 export const Header = () => {
@@ -12,7 +12,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
             <Logo/>
-            <Menu menuItems={items}/>
+            <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -21,6 +21,12 @@ export const Header = () => {
 
 export default Header
 const StyledHeader = styled.header`
-  background-color: #9b6767;
+  background-color: rgba(31, 31, 32, 0.90);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
 
 `;
---
+++ src/layout/header/headermenu/HeaderMenu.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import styled, {StyleSheetManager} from 'styled-components';
+import {theme} from "../../../Styles/Theme";
+
+
+export const HeaderMenu = (props: { menuItems: Array<string> }) => {
+    return (
+        <StyledHeaderMenu>
+            <nav>
+                <ul>
+                    {props.menuItems.map((i, index) => {
+                        return <ListItem key={index}>
+                            <Link href="">{i}
+                                <Mask><span>{i}</span></Mask>
+                                <Mask><span>{i}</span></Mask>
+                            </Link>
+                        </ListItem>
+                    })}
+                </ul>
+            </nav>
+        </StyledHeaderMenu>
+    );
+};
+const StyledHeaderMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 30px;
+  }
+`
+
+const Link = styled.a`
+  color: #7572D5;
+  text-align: center;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  color: transparent;
+`
+const Mask = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 50%;
+  overflow-y: hidden;
+  //outline: 1px solid red;
+  color: ${theme.colors.accent};
+  &+& {
+    top: 50%;
+    span {
+      display: inline-block;
+      transform: translateY(-50%);
+    }
+  }
+`
+const ListItem = styled.li`
+  position: relative;
+  &::before{
+    content:'';
+    display: inline-block;
+    height: 3px;
+    background-color: ${theme.colors.accent};
+    
+    position: absolute;
+    top: 50%;
+    left: -10px;
+    right: -10px;
+    z-index: 1;
+    transform: scale(0);
+  }
+  &:hover{
+    &::before{
+      transform: scale(1);
+    }
+    ${Mask}{
+      transform: skewX(12deg) translateX(5px);
+      color: ${theme.colors.font};
+      & + ${Mask}{
+        transform: skewX(12deg) translateX(-5px);
+
+      }
+    }
+
+  }
+
+`
